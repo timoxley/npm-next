8b79a5156c1a4aa720a821ee4b22aea73dba5a4f
Tim Oxley <secoif@gmail.com>
2014-09-14 05:45:04 +1000

    Upgrade to 2.0.0

--- test.js
+++ test.js
@@ -4,11 +4,11 @@ var test = require('tape')
 var exec = require('child_process').exec
 
 test('uses npm@next', function(t) {
-  exec('npm info npm dist-tags.v2.0-next', function(err, expected) {
+  exec('npm info npm dist-tags.next', function(err, expected) {
     expected = expected.trim()
     exec('./bin/index.js -v', function(err, actual) {
       actual = actual.trim()
-      t.equal(actual, expected, 'uses v2.0-next version')
+      t.equal(actual, expected, 'uses next version')
       t.end()
     })
   })

07aea33e33856265aa39a83a2c210fa0d2965e37
Tim Oxley <secoif@gmail.com>
2016-12-09 15:15:41 +0800

    Exit with correct exit code. Always use version tagged with next

--- bin/index.js
+++ bin/index.js
@@ -4,3 +4,6 @@ var spawn = require('child_process').spawn
 var npm = require.resolve('npm/cli')
 
 spawn(npm, process.argv.slice(2), {stdio: 'inherit'})
+.on('exit', function (code) {
+  process.exit(code)
+})
--- test.js
+++ test.js
@@ -1,12 +1,14 @@
-"use strict"
+'use strict'
 
 var test = require('tape')
 var exec = require('child_process').exec
 
-test('uses npm@next', function(t) {
-  exec('npm info npm dist-tags.next', function(err, expected) {
+test('uses npm@next', function (t) {
+  exec('npm info npm dist-tags.next', function (err, expected) {
+    t.ifError(err)
     expected = expected.trim()
-    exec('./bin/index.js -v', function(err, actual) {
+    exec('./bin/index.js -v', function (err, actual) {
+      t.ifError(err)
       actual = actual.trim()
       t.equal(actual, expected, 'uses next version')
       t.end()
